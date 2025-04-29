7338dccf4219c27356dcf6ed2facf3b013cb3840
Niikawa <nfami8naoto@gmail.com>
2025-04-29 19:38:34 +0900

    メモトップページのみ作成

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TopController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/',[TopController::class,'index'])->name("top");
